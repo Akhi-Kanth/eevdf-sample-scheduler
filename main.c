c88d38a50bd771c3f257aa72eb1fff0dc62f92d7
Akhil Kanthamneni <akneni02@gmail.com>
2026-03-19 16:41:45 -0400

    added functional requests

--- main.c
+++ main.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Request {
     int request_length;
-    int weight;
+    int client_weight;
+};
+
+struct Process {
+    float eligible_time;
+    float eligible_deadline;
+    struct Request currentRequest;
+};
+
+void calculate_ve(struct Process p, int real_time) {
+    
+
+}
+
+struct Request genereate_request() {
+    int random_request_time = (rand() % 5) + 1;
+    int random_weight = (rand() % 5) + 1;
+
+    struct Request random_request;
+    random_request.request_length = random_request_time;
+    random_request.client_weight = random_weight;
+
+    return random_request;
 }
 
 int main () {
-    printf("Hello World");
-    return 0;
+    srand(time(NULL));
+    genereate_request();
 }
